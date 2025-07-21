3efec92a6d5c7cd23648436e0371d0b52adcd0c4
atr86 <atrijroy6443@gmail.com>
2025-07-22 03:21:30 +0530

    Updating bengali

--- modules/bangla_llama.py
+++ modules/bangla_llama.py
@@ -1,47 +1,50 @@
+from llama_cpp import Llama
 import streamlit as st
 import os
-from llama_cpp import Llama
+
 try:
     import gdown
 except ImportError:
         os.system("pip install gdown")
         import gdown
-file_id = "1mulUley1DZw7kMVYbEcCTvOOXBb7uGB1"
+file_id = "1uYktTEF8EaHdYrGkx8DSWe0aPu49P43x"
 url = f"https://drive.google.com/uc?id={file_id}"
-local_model_path="E:/Python/streamlit/v1/Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf"
+local_model_path="llama-3-8b-bangla-GGUF-Q4_K_M-unsloth.Q4_K_M.gguf"
 
 if not os.path.exists(local_model_path):
-    with st.spinner("Model Nous-Hermes-2-Mistral not found locally. Downloading from Google Drive..."):
+    with st.spinner("Model llama-3-8b-bangla not found locally. Downloading from Google Drive..."):
             gdown.download(url, local_model_path, quiet=False)
     st.success("Model downloaded successfully!")
 else:
     print("Model found locally. Using existing file.")
 
-
-
 #  Load GGUF model from local path
 llm = Llama(
-        model_path="Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf",
+        model_path="llama-3-8b-bangla-GGUF-Q4_K_M-unsloth.Q4_K_M.gguf",
         n_ctx=4096,           # Context size (increase if needed)
-        n_threads=6           # Adjust to your CPU cores
+        n_threads=6         # Adjust to your CPU cores
     )
+
 
 def init_for_out():
+    
     from llama_cpp import Llama
+    import streamlit as st
+    import os
     #  Load GGUF model from local path
     llm = Llama(
-        model_path="Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf",
+        model_path="llama-3-8b-bangla-GGUF-Q4_K_M-unsloth.Q4_K_M.gguf",
         n_ctx=4096,           # Context size (increase if needed)
         n_threads=6           # Adjust to your CPU cores
     )
 
-def getResponse(question,context):
+def getResponse(prompt,question,context):
     #  Create a chat-style prompt using create_chat_completion
     response= llm.create_chat_completion(
             messages=[
                 {
-                    "role": "system",#framed in 1 sentence
-                    "content": "Below is a question and a context in bengali language. Frame a simple sentence containing the most matching keyword from the context that directly answers the question. Do not generate more than 1 keyword or more than 1 sentence."
+                    "role": "system",
+                    "content": prompt
                 },
                 {
                     "role": "user",
@@ -49,13 +52,18 @@ def getResponse(question,context):
                 }
             ],
             max_tokens=300,
-            temperature=0.1,  # Min temperature for more deterministic output
+            temperature=0,
             top_p=0.9
         )
     return response["choices"][0]["message"]["content"]
 
+# if __name__ == "__main__":
 
-def run(question,context):   
+
+def run(question,context):
     init_for_out()
-    text=getResponse(question,context)
+    prompt = "Below is a question and a context in bengali language. Frame in a simple sentence containing the most matching keyword from the context that directly answers the question. Do not generate more than 1 keyword or more than 1 sentence."
+    text=getResponse(prompt,question,context)
     return text
+
+
--- modules/qa_module.py
+++ modules/qa_module.py
@@ -1,43 +1,45 @@
+
 def show_qa_tab():
     #from transformers import pipeline
-    import streamlit as st
-    from nous_Mistral import run
-    #from bangla_llama import run as r2
+    import streamlit as st    
 
+    def Nous_Hermes_Mistral_7B(question, context):
+        from modules.nous_Mistral import run
+        return run(question, context)
+    
+    def Bangla_BERT_QA(question, context):
+        from modules.bangla_bert_qa import run
+        return run(question, context)
+    
+    def Bangla_Llama_3_8B(question, context):
+        from modules.bangla_llama import run
+        return run(question, context)
     
 
-    def nousMistral(question, context,gguf):
-        text=run(question, context)
-        return text
-
-    # def bllama(question, answer):
-    #     text=r2(question, answer)
-    #     return text
-    
     #Global var
-    hf_options = []
-    hf_labels = {
-    }
-    hf_fn = {
-    }
-
-    gguf_options=["Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf",]#"bangla-llama-gguf_q4_k_m.gguf"]
-    gguf_labels = {"Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf":"Nous-Hermes-Mistral Q4",
-        #"bangla-llama-gguf_q4_k_m.gguf":"Bangla-llama Q4",
+    hf_options = ["doerig/banglabert"]
+    hf_labels = {"doerig/banglabert": "Bangla-BERT-QA"}
     
+    hf_fn = {"doerig/banglabert": Bangla_BERT_QA}
+
+    gguf_options=["Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf","llama-3-8b-bangla-GGUF-Q4_K_M-unsloth.Q4_K_M.gguf"]
+    gguf_labels = {"Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf":"Nous-Hermes-Mistral Q4",
+        "llama-3-8b-bangla-GGUF-Q4_K_M-unsloth.Q4_K_M.gguf":"Bangla-Llama-Q4"
     }
     gguf_fn = {
-        "Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf":nousMistral,
-        #"bangla-llama-gguf_q4_k_m.gguf":bllama,
+        "Nous-Hermes-2-Mistral-7B-DPO.i1-Q4_K_M.gguf":Nous_Hermes_Mistral_7B,
+       "llama-3-8b-bangla-GGUF-Q4_K_M-unsloth.Q4_K_M.gguf": Bangla_Llama_3_8B
     }   
+    
 
+    
     def qa(category,model_option,context, qs):
         if category == "Original HF models":
             text=hf_fn[model_option](context, qs)
             return text
 
         elif category == "Local GGUF models":
-            text=gguf_fn[model_option](context, qs,gguf=True)
+            text=gguf_fn[model_option](context, qs)
             return text
 
     #Select model
@@ -73,13 +75,13 @@ def show_qa_tab():
 
 
     context=st.session_state.text
-    #st.text_area(label=f"Your transcribed text is",value=context,height=100)
-    st.code(context, language="text", height=150)
+    st.text_area(label=f"Your transcribed text is",value=context,height=100)
+    #st.code(context, language="text", height=150)
 
 
     # Your question
     question = st.text_input("Enter your question")
-    st.code(question,language="text",height=65)
+    #st.code(question,language="text",height=65)
 
     if st.button(f"Answer", key=f"btn2{0}"):
     # Perform extractive QA
@@ -87,13 +89,17 @@ def show_qa_tab():
         result = qa(selected_category, selected_model, question,context)
 
         # Print answer
-        #st.text_area(f"Answer:",value=result,height=68)
-        st.code(result, language="text",height=100)
+        st.text_area(f"Answer:",value=result,height=68)
+        #st.code(result, language="text",height=100)
         #st.text_area("More Details:",value=result,height=68)
         #print(type(result)) of  dict type
 
     else:
         st.info("Please enter question and click Answer button")
 
-    # def qa(selected_category, selected_model, question, context):
-    #     return nousMistral(question,context)
+
+
+
+
+
+
